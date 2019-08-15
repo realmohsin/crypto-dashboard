f13a4477aa13d14475886b226f952e06b7a75697
Real Mohsin <realmohsin@outlook.com>
2019-08-15 14:52:49 -0400

    add navbar and grid

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { css } from '@emotion/core'
 import PageLayout from '../components/PageLayout'
 import WelcomeMessage from '../components/WelcomeMessage'
+import Navbar from '../components/Navbar'
 
 const HomePage = props => {
   return (
     <PageLayout>
+      <Navbar />
       <WelcomeMessage />
     </PageLayout>
   )
